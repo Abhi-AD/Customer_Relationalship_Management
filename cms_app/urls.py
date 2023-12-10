38c03f74f4236d638d78c3b393d17502b5224ccf
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2023-12-10 16:49:59 +0545

    feat;add the template

---
+++ cms_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from cms_app import views
+
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path("add_member/", views.AddMember.as_view(), name="add_member"),
+]
